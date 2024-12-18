a207c27ec6e2c5476318eb05594c544fc3d5df63
maria <marigome@marialinux.es>
2024-12-18 09:01:20 +0100

    impar fixed

--- philo_bonus/src/manage_philos_bonus.c
+++ philo_bonus/src/manage_philos_bonus.c
@@ -6,7 +6,7 @@
 /*   By: marigome <marigome@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 11:43:52 by marigome          #+#    #+#             */
-/*   Updated: 2024/12/18 08:20:24 by marigome         ###   ########.fr       */
+/*   Updated: 2024/12/18 09:00:00 by marigome         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,6 @@ void	*ft_check_death(void *arg)
 		sem_wait(philo->data->death);
 		if (philo->data->current_eat != -1 && philo->eat_count >= philo->data->max_eat)
 		{
-			ft_print_message(DONE, philo);
 			sem_post(philo->data->stop);
 			break ;
 		}
@@ -39,52 +38,59 @@ void	*ft_check_death(void *arg)
 	return (NULL);
 }
 
+/*void	ft_sleep(unsigned long time, t_data *data)
+{
+	unsigned long	start;
 
+	start = ft_get_time();
+	while (!data->stop)
+	{
+		if (ft_get_time() - start >= time)
+			break ;
+		usleep(data->philo_count * 3);
+	}
+}*/
 void	ft_take_fork(t_philo *philo)
 {
-	sem_wait(philo->data->forks);
-	ft_print_message("Has taken a fork", philo);
-	sem_wait(philo->data->forks);
-	ft_print_message("Has taken a fork", philo);
+	if (philo->id % 2 == 0)
+	{
+		sem_wait(philo->data->forks); // Toma el primer tenedor
+		ft_print_message(TAKEN_FORK, philo);
+		sem_wait(philo->data->forks); // Toma el segundo tenedor
+		ft_print_message(TAKEN_FORK, philo);
+	}
+	else
+	{
+		sem_wait(philo->data->forks); // Toma el primer tenedor
+		ft_print_message(TAKEN_FORK, philo);
+		usleep(100); // Breve retraso para evitar bloqueos
+		sem_wait(philo->data->forks); // Toma el segundo tenedor
+		ft_print_message(TAKEN_FORK, philo);
+	}
 }
 
-/*void	ft_eat(t_philo *philo)
-{
-	ft_print_message(EAT, philo);
-	if (philo->data->eat_counter != -1)
-		philo->data->current_eat++;
-	usleep(philo->data->time_to_eat * 1000);
-	philo->last_meal = ft_get_time();
-	philo->next_meal = philo->last_meal
-		+ (unsigned int)philo->data->time_to_die;
-	sem_post(philo->data->forks);
-	sem_post(philo->data->forks);
-}*/
 
 void	ft_eat(t_philo *philo)
 {
-	// Tomar los tenedores
-	sem_wait(philo->data->forks); // Tomar primer tenedor
-	ft_print_message(TAKEN_FORK, philo);
-	sem_wait(philo->data->forks); // Tomar segundo tenedor
-	ft_print_message(TAKEN_FORK, philo);
+	/*sem_wait(philo->data->forks);
+	sem_wait(philo->data->forks); // Tomar segundo tenedor*/
+	ft_take_fork(philo); // Tomar ambos tenedores
 
 	// Actualizar el estado del filósofo a "EAT"
 	sem_wait(philo->data->death); // Proteger acceso a las variables críticas
 	ft_print_message(EAT, philo);
 
 	philo->last_meal = ft_get_time(); // Registrar tiempo de última comida
-	philo->next_meal = philo->last_meal + philo->data->time_to_die;
+	philo->next_meal = philo->last_meal + (unsigned int)philo->data->time_to_die;
 
 	philo->eat_count++; // Incrementar el contador individual de comidas
-	printf("Filosofo: %d, Comidas: %d\n", philo->id, philo->eat_count);
 	if (philo->data->eat_counter != -1 && philo->eat_count >= philo->data->eat_counter)
 		philo->data->current_eat++; // Actualizar contador global
 
 	sem_post(philo->data->death); // Desbloquear acceso a las variables críticas
 
 	// Simular el tiempo de comer
-	usleep(philo->data->time_to_eat * 1000);
+	usleep(philo->data->time_to_sleep * 400);
 
 	// Devolver los tenedores
 	sem_post(philo->data->forks); // Liberar primer tenedor
@@ -96,7 +102,9 @@ void	ft_eat(t_philo *philo)
 void	ft_sleep(t_philo *philo)
 {
 	ft_print_message(SLEEP, philo);
-	usleep(philo->data->time_to_sleep * 1000);
+	if (philo->id % 2 && philo->data->philo_count > 1)
+		usleep(philo->data->time_to_eat * 1000);
+	usleep(philo->data->philo_count * 1000);
 }
 
 void	ft_routine(t_philo *philo)
@@ -111,6 +119,7 @@ void	ft_routine(t_philo *philo)
 		//ft_take_fork(philo);
 		ft_eat(philo);
 		ft_sleep(philo);
-		//ft_print_message(THINK, philo);
+		ft_print_message(THINK, philo);
+		usleep(500);
 	}
 }
