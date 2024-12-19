e6f655adc4480b909a5d8b93b9e7b8995a44bab8
marigome <marigome@student.42malaga.com>
2024-12-19 12:25:28 +0100

    norminette ok

--- philo/src/free.c
+++ philo/src/free.c
@@ -6,7 +6,7 @@
 /*   By: marigome <marigome@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 10:09:23 by marigome          #+#    #+#             */
-/*   Updated: 2024/12/13 11:38:49 by marigome         ###   ########.fr       */
+/*   Updated: 2024/12/19 12:24:56 by marigome         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,3 @@ void	ft_free_philo(int i, t_data *env)
 	while (i-- >= 0)
 		free(env->philos[i].status);
 }
-
